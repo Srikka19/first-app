42f1a76cd5e45a2e5f9c47292ae14c06bd1b4124
Srikka S <Srikka@sris-air.lan>
2019-01-12 16:57:27 +0000

    firstCommit

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,38 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import {HomeComponent} from './home/home.component';
+import {AboutMeComponent} from './about-me/about-me.component';
+import {ResumeComponent} from './resume/resume.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+{
+  path: 'home',
+  component: HomeComponent
+},
+{
+  path: 'about-me',
+  component: AboutMeComponent
+},
+{
+  path: 'resume',
+  component: ResumeComponent
+},
+{
+  path: '',
+  redirectTo: '/home',
+  pathMatch: 'full'
+},
+{
+  path: '**',
+  redirectTo: '/home',
+  pathMatch: 'full'
+}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
   exports: [RouterModule]
 })
-export class AppRoutingModule { }
+
+
+export class AppRoutingModule {}
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,18 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { NavigationBarComponent } from './navigation-bar/navigation-bar.component';
+import { HomeComponent } from './home/home.component';
+import { AboutMeComponent } from './about-me/about-me.component';
+import { ResumeComponent } from './resume/resume.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavigationBarComponent,
+    HomeComponent,
+    AboutMeComponent,
+    ResumeComponent,
   ],
   imports: [
     BrowserModule,
